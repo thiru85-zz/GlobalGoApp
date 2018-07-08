2db9ad8047baf097e9ffa52f8a2fdd5fac7ca5bb
Thirumalai Aiyalu <thirumalai@thirumalaiaiyalu.com>
2018-07-08 16:53:04 +0800

    Added some code

--- main.go
+++ main.go
@@ -49,10 +49,11 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, IOxKL!!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Container/Pod Name: %s\n", host)
-	client := &http.Client{}
+	client := &http.Client{
     	req, err := http.NewRequest("GET", "http://metadata.google.internal/computeMetadata/v1/instance/hostname", nil)
 		req.Header.Add("Metadata-Flavor", "Google")
 		resp, err := client.Do(req)
+	}
 	fmt.Fprintf(w, "K8s Node Name: %s \n", client)
 	}
 // [END all]
